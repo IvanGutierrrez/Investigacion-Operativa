4c6330331ce8ef70340ac03bc2cff4123d2be08e
IvanGutierrrez <146970754+IvanGutierrrez@users.noreply.github.com>
2025-01-10 21:45:40 +0100

    Update Practica2.py

--- Práctica2/Practica2.py
+++ Práctica2/Practica2.py
@@ -22,6 +22,42 @@ def mm1_measures(lam, mu):
         'Wq': Wq
     }
 
+def mm1k_measures(lam, mu, K):
+    rho = lam / mu
+    # Verificar si rho es igual a 1
+    if rho == 1:
+        S = K + 1
+    else:
+        S = (1 - rho ** (K + 1)) / (1 - rho)
+    # Calcular Pn
+    pn_dict = {}
+    for n in range(K + 1):
+        Pn = (rho ** n) / S
+        pn_dict[n] = Pn
+    # Probabilidad de que el sistema esté lleno (PK)
+    PK = pn_dict[K]
+    # Tasa efectiva de llegada
+    lam_e = lam * (1 - PK)
+    # Número promedio de clientes en el sistema (L)
+    L = sum(n * pn_dict[n] for n in range(K + 1))
+    # Tiempo promedio en el sistema (W)
+    W = L / lam_e if lam_e > 0 else 0
+    # Tiempo promedio en cola (Wq)
+    Wq = W - (1 / mu)
+    # Número promedio de clientes en cola (Lq)
+    Lq = L - (1 - pn_dict[0])
+    return {
+        'lambda': lam,
+        'lambdae': lam_e,
+        'mu': mu,
+        'rho': rho,
+        'L': L,
+        'Lq': Lq,
+        'W': W,
+        'Wq': Wq,
+        'Pk' : PK
+    }
+
 # Función para imprimir resultados
 def print_results(department, measures):
     print(f"\nResultados para el Nodo {department}:")
@@ -32,6 +68,18 @@ def print_results(department, measures):
     print(f"Número promedio en cola (Lq): {measures['Lq']:.4f} clientes")
     print(f"Tiempo promedio en el sistema (W): {measures['W']:.4f} horas ({measures['W']*60:.2f} minutos)")
     print(f"Tiempo promedio en cola (Wq): {measures['Wq']:.4f} horas ({measures['Wq']*60:.2f} minutos)")
+
+def print_resultsK(department, measures):
+    print(f"\nResultados para el Nodo {department}:")
+    print(f"Tasa de llegada (lambda): {measures['lambda']:.4f} clientes/hora")
+    print(f"Tasa de efectiva de llegada (lambdae): {measures['lambdae']:.4f} clientes/hora")
+    print(f"Tasa de servicio (mu): {measures['mu']:.4f} clientes/hora")
+    print(f"Utilización (rho): {measures['rho']:.4f}")
+    print(f"Número promedio en el sistema (L): {measures['L']:.4f} clientes")
+    print(f"Número promedio en cola (Lq): {measures['Lq']:.4f} clientes")
+    print(f"Tiempo promedio en el sistema (W): {measures['W']:.4f} horas ({measures['W']*60:.2f} minutos)")
+    print(f"Tiempo promedio en cola (Wq): {measures['Wq']:.4f} horas ({measures['Wq']*60:.2f} minutos)")
+    print(f"Probabilidad de que el sistema esté lleno (PK): {measures['Pk']:.4f}%")
 
 def move_nodes(service_start_times, service_end_times, estadoNodos, service_times, probs):
     datos = []
@@ -116,7 +164,7 @@ def simulate_priority_queue_MM1(lambda_rate, mu_rate, num_customers, priority_le
                                 break
                         if not ok: #Si el next_customer no tiene valor
                             next_customer = None
-                            break
+                            continue
                         if p == 1: # baja
                             instancias_cola_baja[next_customer] = len(queues[p])
                             instancias_cola_alta[next_customer] = -1
@@ -125,7 +173,7 @@ def simulate_priority_queue_MM1(lambda_rate, mu_rate, num_customers, priority_le
                             instancias_cola_baja[next_customer] = -1
                         break
 
-                if next_customer == None:
+                if not next_customer or next_customer == None:
                     continue
 
                 # Asignar tiempos de servicio
@@ -157,9 +205,6 @@ def simulate_priority_queue_MM1(lambda_rate, mu_rate, num_customers, priority_le
             system_times.append(-1)
 
     # Calcular métricas
-    # average_wait = np.mean(wait_times)
-    # average_system_time = np.mean(system_times)
-
     # Filtrar valores 0 de service_end_times
     filtered_service_end_times = [
         [t for t in times if t > 0] for times in service_end_times
@@ -176,28 +221,41 @@ def calcularClientes(arrival_times,service_end_times,actualTime,priorities,estad
     high_priority_indices = np.where(priorities == 0)[0]
     low_priority_indices = np.where(priorities == 1)[0]
 
-    service_end_times_high = service_end_times[high_priority_indices]
+    service_end_times_high = service_end_times[0][high_priority_indices]
     arrival_times_high = arrival_times[high_priority_indices]
 
-    service_end_times_low = service_end_times[low_priority_indices]
+    service_end_times_low = service_end_times[0][low_priority_indices]
     arrival_times_low = arrival_times[low_priority_indices]
+
+    filtered_service_end_times_high = [
+        [t for t in service_end_times_high if t > 0]
+    ]
+    filtered_arrival_times_high = [
+        [t for t in arrival_times_high if t > 0]
+    ]
+    filtered_service_end_times_low = [
+        [t for t in service_end_times_low if t > 0]
+    ]
+    filtered_arrival_times_low = [
+        [t for t in arrival_times_low if t > 0]
+    ]
 
     sum = [0, 0, 0]
     i = 0 #El último cliente terminado
-    while i < len(service_end_times_high) and service_end_times_high[i] != 0:
+    while i < len(filtered_service_end_times_high) and service_end_times_high[i] != 0:
         i += 1
-    if i < len(service_end_times_high):
-        for j in range(i,len(arrival_times_high)):
-            if 0 < arrival_times_high[j] <= actualTime:
+    if i < len(filtered_service_end_times_high):
+        for j in range(i,len(filtered_arrival_times_high)):
+            if 0 < filtered_arrival_times_high[j][0] <= actualTime:
                 sum[0] += 1
             else:
                 break
     i = 0  # El último cliente terminado
-    while i < len(service_end_times_low) and service_end_times_low[i] != 0:
+    while i < len(filtered_service_end_times_low) and filtered_service_end_times_low[i] != 0:
         i += 1
-    if i < len(service_end_times_low):
-        for j in range(i, len(arrival_times_low)):
-            if 0 < arrival_times_low[j] <= actualTime:
+    if i < len(filtered_service_end_times_low):
+        for j in range(i, len(filtered_arrival_times_low)):
+            if 0 < filtered_arrival_times_low[j][0] <= actualTime:
                 sum[1] += 1
             else:
                 break
@@ -205,24 +263,14 @@ def calcularClientes(arrival_times,service_end_times,actualTime,priorities,estad
         if i != -1:
             sum[2] += 1
     return sum
-1
-def modeloPaciencia(queues, arrival_times,priority_levels,maxTime,actualTime):
-    listaAbandonos = []
-    for q in range(priority_levels):
-        if queues[q]:
-            for customer in queues[q]:
-                if actualTime - arrival_times[customer] > maxTime:
-                    listaAbandonos.append(customer)
-                    queues[q].remove(customer)
-    return listaAbandonos
 
 def simulate_priority_queue_MM1K(lambda_rate, mu_rate, num_customers, priority_levels, probs, K, maxTime):
     # Generar tiempos entre llegadas y tiempos de servicio
     inter_arrival_times = np.random.exponential(1 / lambda_rate, num_customers)
 
-    service_times = [[], [], [], [], []]
+    service_times = []
     for i in range(5):
-        service_times[i] = np.random.exponential(1 / mu_rate[i], num_customers)
+        service_times.append(np.random.exponential(1 / mu_rate[i], num_customers))
 
     priorities = np.random.choice(range(priority_levels), size=num_customers, p=[0.7, 0.3])
 
@@ -249,7 +297,7 @@ def simulate_priority_queue_MM1K(lambda_rate, mu_rate, num_customers, priority_l
     estadoNodos = [-1, -1, -1, -1, -1]
 
     #
-    abandonadosPorTiempo = [] #Abandonados por tiempo
+    abandonadosPorTiempo = 0 #Abandonados por tiempo
     abandonadosPorBloqueo = 0 #Abandonados por bloqueo
     # Inicializar índice de llegada
     i = 0
@@ -259,43 +307,51 @@ def simulate_priority_queue_MM1K(lambda_rate, mu_rate, num_customers, priority_l
             sum = calcularClientes(arrival_times, service_end_times, actualTime, priorities, estadoNodos)
             if sum[0] + sum[1] + sum[2] <= K:  # Si hay espacio en el sistema
                 queues[priorities[i]].append(i)
-                i += 1
             else:
                 # Asignar tiempos de servicio negativos, ya que no se atiende
-                service_start_times = np.delete(service_start_times, i)
-                wait_times = np.delete(wait_times, i)
-                service_end_times = np.delete(service_end_times, i)
-                arrival_times = np.delete(arrival_times, i)
-                service_times = np.delete(service_times, i)
-                priorities = np.delete(priorities, i)
+                for j in range(5):
+                    service_start_times[j][i] = -1
+                    service_end_times[j][i] = -1
+                    service_times[j][i] = -1
+                wait_times[i] = -1
+                arrival_times[i] = -1
+                priorities[i] = -1
                 abandonadosPorBloqueo += 1
-                num_customers -= 1
-
-            #Comprobar tiempo máximo
-            listaAbandonos = modeloPaciencia(queues,arrival_times,priority_levels,maxTime, actualTime)
-            if any(listaAbandonos):
-                for j in listaAbandonos:
-                    service_start_times = np.delete(service_start_times, j)
-                    wait_times = np.delete(wait_times, j)
-                    service_end_times = np.delete(service_end_times, j)
-                    arrival_times = np.delete(arrival_times, j)
-                    service_times = np.delete(service_times, j)
-                    priorities = np.delete(priorities, j)
-                    abandonadosPorTiempo += 1
-                    num_customers -= 1
+            i += 1
         else:
             if any(queues[p] for p in range(priority_levels)):
                 # Encontrar la cola con la más alta prioridad disponible
+                ok = False
                 for p in range(priority_levels):
                     if queues[p]:
-                        next_customer = queues[p].popleft()
-                        if p == 1: # baja
+                        while queues[p]:
+                            next_customer = queues[p].popleft()
+                            # Comprobar tiempo máximo
+                            if actualTime - arrival_times[next_customer] > maxTime:
+                                for j in range(5):
+                                    service_start_times[j][next_customer] = -1
+                                    service_end_times[j][next_customer] = -1
+                                    service_times[j][next_customer] = -1
+                                wait_times[next_customer] = -1
+                                arrival_times[next_customer] = -1
+                                priorities[next_customer] = -1
+                                abandonadosPorTiempo += 1
+                            else:
+                                ok = True
+                                break
+                        if not ok:  # Si el next_customer no tiene valor
+                            next_customer = None
+                            continue
+                        if p == 1:  # baja
                             instancias_cola_baja[next_customer] = len(queues[p])
                             instancias_cola_alta[next_customer] = -1
                         else:
                             instancias_cola_alta[next_customer] = len(queues[p])
                             instancias_cola_baja[next_customer] = -1
                         break
+
+                if not next_customer or next_customer == None:
+                    continue
 
                 # Asignar tiempos de servicio
                 service_start_times[0][next_customer] = max(arrival_times[next_customer], actualTime)
@@ -316,18 +372,23 @@ def simulate_priority_queue_MM1K(lambda_rate, mu_rate, num_customers, priority_l
     # Calcular tiempos en el sistema
     system_times = []
     for i in range(num_customers):
-        final_servicio = service_end_times[0][i]
-        for j in range(1,5):
-            if final_servicio < service_end_times[j][i]:
-                final_servicio = service_end_times[j][i]
-        system_times.append(final_servicio - arrival_times[i])
+        if service_times[0][i] != -1:
+            final_servicio = service_end_times[0][i]
+            for j in range(1, 5):
+                if final_servicio < service_end_times[j][i]:
+                    final_servicio = service_end_times[j][i]
+            system_times.append(final_servicio - arrival_times[i])
+        else:
+            system_times.append(-1)
 
     # Calcular métricas
     # Filtrar valores 0 de service_end_times
     filtered_service_end_times = [
         [t for t in times if t > 0] for times in service_end_times
     ]
-
+    filtered_service_times = [
+        [t for t in times if t > 0] for times in service_times
+    ]
     server_utilization = [np.sum(service_times[i]) / filtered_service_end_times[i][-1] if filtered_service_end_times[i][-1] > 0 else 0 for i in range(5)]
 
     return wait_times, system_times, priorities, server_utilization, instancias_cola_baja, instancias_cola_alta, abandonadosPorBloqueo, abandonadosPorTiempo
@@ -358,6 +419,7 @@ if __name__ == '__main__':
             # Cálculo de medidas para cada departamento
             measures = [mm1_measures(landa, mu[i]) for i in range(5)]
             # Imprimir resultados
+            print("\nValores matemáticos: \n")
             for i in range(5):
                 print_results(str(i), measures[i])
 
@@ -381,6 +443,17 @@ if __name__ == '__main__':
                          list(map(float, lines[7].split()))
                          ]
                 maxTime = float(lines[8])
+
+            # Datos teóricos
+            # Datos del sistema
+            # Cálculo de las utilizaciones
+            rho = [landa / mu[i] for i in range(5)]
+            # Cálculo de medidas para cada departamento
+            measures = [mm1k_measures(landa, mu[i],k) for i in range(5)]
+            # Imprimir resultados
+            print("\nValores matemáticos: \n")
+            for i in range(5):
+                print_resultsK(str(i), measures[i])
 
             #Simulación
             # Parámetros
@@ -437,7 +510,7 @@ if __name__ == '__main__':
             print(f"Utilización del servidor en el nodo {i}: {utilization[i]:.2%}\n", end= '')
         print(f"Tasa de abandono: {abandonados / num_customers:.4f}")
         if entrada == 2:
-            print(f"Probabilidad de bloqueo: {abandonadosPorBloqueo / num_customers * 100:.4f}%")
+            print(f"Probabilidad de bloqueo: {abandonadosPorBloqueo / num_customers * 100:.2f}%")
         print()
 
         # Imprimir métricas por prioridad
@@ -472,3 +545,5 @@ if __name__ == '__main__':
         plt.legend()
         plt.grid(True)
         plt.show()
+
+        break
